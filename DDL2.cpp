3377ab380decdbb0575b8325746fc3c239a4757a
Nick <nikhilsoniya8@gmail.com>
2022-10-11 01:16:09 +0530

    add checklocation function, pending insertion at particular node

--- DDL2.cpp
+++ DDL2.cpp
@@ -12,67 +12,98 @@ struct Node
 struct Node *head = NULL;
 struct Node *link = NULL;
 
-//function
+// function
 void insert(int type);
 void display(int type);
 void DisLines();
+int checklocation(char loc);
 int count_Node();
 
 // main function
 int main()
 {
-	
-	dashboard:
+
+dashboard:
 
 	int usr_choice;
 
-	cout<<endl<<"Dashboard:- Press following key to perform Operation: ";
-	cout<<endl<<"Create Linklist/Insert Operations: \n\t1- Insert at the end\n\t2- Insert at start\n\t3-Insert after Particular Node";
-	cout<<endl<<"Display Operations: \n\t4- Display Forward\n\t5- Display Reverse";
-	cout<<endl<<"Delete Operations: \n\t6- Delete first node\n\t7- Delete last node\n\t8- Delete a Particular node";
-	cout<<endl<<"9- Exit dashboard: \n";
-	cin>>usr_choice;
+	cout << endl
+		 << "Dashboard:- Press following key to perform Operation: ";
+	cout << endl
+		 << "Create Linklist/Insert Operations: \n\t1- Insert at the end\n\t2- Insert at start\n\t3-Insert after Particular Node";
+	cout << endl
+		 << "Display Operations: \n\t4- Display Forward\n\t5- Display Reverse";
+	cout << endl
+		 << "Delete Operations: \n\t6- Delete first node\n\t7- Delete last node\n\t8- Delete a Particular node";
+	cout << endl
+		 << "9- Exit dashboard: \n";
+	cin >> usr_choice;
 
-	switch(usr_choice){
-		case 1: insert(0);goto dashboard;break; 
-		case 2: insert(1);goto dashboard;break; 
-		case 3: insert(2);goto dashboard;break; 
-		case 4: display(0);goto dashboard;break; 
-		case 5: display(1);goto dashboard;break;
-		case 9: break;
-		default: cout<<endl<<"You've entered other then following key";
-	}	
-	
+	switch (usr_choice)
+	{
+	case 1:
+		insert(0);
+		goto dashboard;
+		break;
+	case 2:
+		insert(1);
+		goto dashboard;
+		break;
+	case 3:
+		insert(2);
+		goto dashboard;
+		break;
+	case 4:
+		display(0);
+		goto dashboard;
+		break;
+	case 5:
+		display(1);
+		goto dashboard;
+		break;
+	case 9:
+		break;
+	default:
+		cout << endl
+			 << "You've entered other then following key";
+	}
 
 	return 0;
 }
 
-
-//Insertion function
-void insert(int type){
+// Insertion function
+void insert(int type)
+{
 	/*	type 0 - insert at the end
-	 	type 1 - insert at begining
-	 	type 2 - insert after particular node
+		type 1 - insert at begining
+		type 2 - insert after particular node
 	*/
-	
-	//variables
+
+	// variables
 	char userdata;
 	int n;
 
-	if(type == 0){
+	if (type == 0)
+	{
 
-		cout<<"[Insertion at End]: Enter number of node you want to insert: ";
-		cin>>n;
+		cout << "[Insertion at End]: Enter number of node you want to insert: ";
+		cin >> n;
 
-		if(n<=0){
-			cout<<endl<<"You've to insert atleast 1 node in linklist";
-		}else{
+		if (n <= 0)
+		{
+			cout << endl
+				 << "You've to insert atleast 1 node in linklist";
+		}
+		else
+		{
 
-			for(int i = 1;i<=n;i++){
+			for (int i = 1; i <= n; i++)
+			{
 
-				cout << endl << "Enter data for "<<i<<": ";
+				cout << endl
+					 << "Enter data for " << i << ": ";
 				cin >> userdata;
-				
+
 				if (head == NULL)
 				{
 					struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
@@ -91,24 +122,30 @@ void insert(int type){
 					ptr->prev = link;
 					link = link->next;
 				}
-
 			}
-
 		}
-	}else if(type == 1){
+	}
+	else if (type == 1)
+	{
 
-		cout<<"[Insertion at start]: Enter number of node you want to insert: ";
-		cin>>n;
+		cout << "[Insertion at start]: Enter number of node you want to insert: ";
+		cin >> n;
 
-		if(n<=0){
-			cout<<endl<<"You've to insert atleast 1 node in linklist";
-		}else{
+		if (n <= 0)
+		{
+			cout << endl
+				 << "You've to insert atleast 1 node in linklist";
+		}
+		else
+		{
 
-			for(int i = 1;i<=n;i++){
+			for (int i = 1; i <= n; i++)
+			{
 
-				cout << endl << "Enter data for "<<i<<": ";
+				cout << endl
+					 << "Enter data for " << i << ": ";
 				cin >> userdata;
-				
+
 				if (head == NULL)
 				{
 					struct Node *ptRRR = (struct Node *)malloc(sizeof(struct Node));
@@ -119,74 +156,155 @@ void insert(int type){
 					link = ptRRR;
 				}
 				else
-				{	
+				{
 					struct Node *ptRRR = (struct Node *)malloc(sizeof(struct Node));
 					struct Node *se = head;
 
-					while(se->prev != NULL){
+					while (se->prev != NULL)
+					{
 						se = se->prev;
 					}
 					ptRRR->data = userdata;
 					se->prev = ptRRR;
 					ptRRR->next = se;
-					ptRRR->prev = NULL; 
+					ptRRR->prev = NULL;
 					head = ptRRR;
-
 				}
-
 			}
-
 		}
 	}
+	else if (type == 2)
+	{
+		char loc;
+		cout << "[Search data]: Enter The location data:  ";
+		cin >> loc;
+		if (checklocation(loc) == 0)
+		{
+			cout << endl
+				 << "[new head]: Enter data: ";
+			cin >> userdata;
 
-}
-void DisLines(){
-	for(int i = 0; i<=count_Node(); i++){
-		cout<<"--";
+			if (head == NULL)
+			{
+				struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+				ptr->data = userdata;
+				ptr->next = NULL;
+				ptr->prev = NULL;
+				head = ptr;
+				link = ptr;
+			}
+			else
+			{
+				struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+				struct Node *findloc = head;
+				while (findloc->data != loc)
+				{
+					findloc = findloc->next;
+				}
+				ptr->data = userdata;
+				findloc->next->prev = ptr;
+				ptr->next = findloc->next;
+				findloc->next = ptr;
+				ptr->prev = findloc;
+						}
+		}
 	}
-	cout<<endl;
 }
-void display(int type){
+
+void DisLines()
+{
+	for (int i = 0; i <= count_Node(); i++)
+	{
+		cout << "--";
+	}
+	cout << endl;
+}
+
+void display(int type)
+{
 	/*
 		type 0 - Display Forward
 		type 1 - Display Reverse
 	*/
-		system("cls");
-	if(type == 0){
+	system("cls");
+	if (type == 0)
+	{
 		struct Node *ptr = head;
-		//display lines
-		DisLines();	
-			while (ptr != NULL)
-			{
-				cout << ptr->data << " ";
-				ptr = ptr->next;
-			}
-		cout<<endl;
+		// display lines
 		DisLines();
-		delete(ptr);
-	}else if(type == 1){
+		while (ptr != NULL)
+		{
+			cout << ptr->data << " ";
+			ptr = ptr->next;
+		}
+		cout << endl;
+		DisLines();
+		delete (ptr);
+	}
+	else if (type == 1)
+	{
 		struct Node *pTr = link;
 
-		DisLines();	
+		DisLines();
 		while (pTr != NULL)
 		{
-			cout<<pTr->data<<" ";
+			cout << pTr->data << " ";
 			pTr = pTr->prev;
 		}
-		cout<<endl;
-		DisLines();	
-		delete(pTr);
-
+		cout << endl;
+		DisLines();
+		delete (pTr);
 	}
 }
 
-int count_Node(){
+// Count number of node present in linklist
+int count_Node()
+{
 	int count = 0;
-struct Node *ppt = head;
-	while(ppt !=NULL){
+	struct Node *ppt = head;
+	while (ppt != NULL)
+	{
 		count++;
 		ppt = ppt->next;
 	}
-	delete(ppt);
+	delete (ppt);
 	return count;
+}
+
+// checklocation function
+int checklocation(char loc)
+{
+	bool islocate = false;
+	struct Node *ptr = head;
+
+	if (ptr == NULL)
+	{
+		cout << "\n------------------\n";
+		cout << "Linklist is empty";
+		cout << "\n------------------\n";
+	}
+	else
+	{
+
+		while (ptr != NULL)
+		{
+			if (ptr->data == loc)
+			{
+				islocate = false;
+				break;
+			}
+			else
+			{
+				islocate = true;
+			}
+			ptr = ptr->next;
+		}
+		if (islocate == 1)
+		{
+			cout << "\n--------------------------------------\n";
+			cout << "Couldn't found Data '" << loc << "' in linklist";
+			cout << "\n--------------------------------------\n";
+		}
+	}
+	return islocate;
 }
